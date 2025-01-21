cd6652cc7b069807c95d3dd7a179c90e914e20f6
Max Dubakov <max@fibery.io>
2025-01-21 21:21:32 +0100

    Connect ruff linter & formatter

--- compotational_domain.py
+++ compotational_domain.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 
 # -------- basic example -----------------------------------------------------------------------------------------------
 
-x, y, z = 1, 2, 3 # some Python boxed primitive variables
+x, y, z = 1, 2, 3  # some Python boxed primitive variables
 
 # oh, I need to multiply each of them by 2
 
@@ -11,14 +11,13 @@ x, y, z = 1, 2, 3 # some Python boxed primitive variables
 values = np.array([x, y, z])
 
 # 2. compute
-values *= 2 # here, numpy operates on the level of unboxed variables, i.e. int64 in C. So, it can parallalize it as it thinks optimal
+values *= 2  # numpy operates on the level of unboxed variables, i.e. int64 in C. So, it can parallelize it optimally
 
 # 3. put it back to boxed
 x, y, z = values
 
 
 # -------- pd.DataFrame & class example --------------------------------------------------------------------------------
-
 class Resistor:
     def __init__(self, number, manufacturer, resistance):
         self.number = number
@@ -29,21 +28,22 @@ class Resistor:
 class Product:
     def __init__(self, *components):
         # index=[...] actually puts these in computational domain where it's implemented as numpy array
-        self.components = DataFrame([
-            [c.manufacturer, c.resistance]
-            for c in components
-        ], columns=['manufacturer', 'resistance'], index=[c.number for c in components])
+        self.components = DataFrame(
+            [[c.manufacturer, c.resistance] for c in components],
+            columns=["manufacturer", "resistance"],
+            index=[c.number for c in components],
+        )
 
     def __getitem__(self, number):
-        c = self.components.loc[number] # using computational domain to efficiently get item
-        return Resistor(number, c.manufacturer, c.resistance) # back to boxed Python objects
+        c = self.components.loc[number]  # using computational domain to efficiently get item
+        return Resistor(number, c.manufacturer, c.resistance)  # back to boxed Python objects
 
 
 p = Product(
-    Resistor('10-1', 'honhai', 1),
-    Resistor('10-2', 'honhai', 5),
-    Resistor('10-3', 'honhai', 10),
+    Resistor("10-1", "honhai", 1),
+    Resistor("10-2", "honhai", 5),
+    Resistor("10-3", "honhai", 10),
 )
 
-print(f'{p.components.resistance.mean() = }')
-print(f'{p["10-1"]}')
+print(f"{p.components.resistance.mean()=}")
+print(f"{p['10-1']}")
